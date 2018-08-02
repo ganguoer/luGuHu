58852d49ff2c685de82db2b38dcf6e4593fdedbf
ganguoer <ganguoer_dan@163.com>
2018-08-02 18:57:32 +0800

    second

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import my from '@/components/my/my'   //我的页面
+import order from '@/components/my/order'   //我的订单页面
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/my',
       name: 'my',
       component: my
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: order
     }
   ]
 })
