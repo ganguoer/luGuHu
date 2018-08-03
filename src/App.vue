f61fc4f07ffcc8beb8a0f5afe73d629db966face
ganguoer <ganguoer_dan@163.com>
2018-08-03 18:41:03 +0800

    forth

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,28 @@
 <template>
   <div id="app">
 
-     <!-- <header :headTitle="headMsg"></header> -->
-    <router-view/>
-    <mt-header fixed title="我的"></mt-header>
+    <mt-header fixed title="泸沽湖"></mt-header>
 
     <div class="content">
       <router-view/>
     </div>
 
-<!--底部导航-->
-      <mt-tabbar v-model="selected">
-        <mt-tab-item id="index">
-          <img slot="icon" src="../static/images/1.png">
-          首页
-        </mt-tab-item>
-        <mt-tab-item id="order">
-          <img slot="icon" src="../static/images/1.png">
-          资讯
-        </mt-tab-item>
-        <mt-tab-item id="market">
-          <img slot="icon" src="../static/images/1.png">
-          微商城
-        </mt-tab-item>
-        <mt-tab-item id="guide">
-          <img slot="icon" src="../static/images/1.png">
-          自导
-        </mt-tab-item>
-        <mt-tab-item id="my">
-          <img slot="icon" src="../static/images/1.png">
-          我的
-        </mt-tab-item>
-      </mt-tabbar>
-
-
-  </div>
+    <bottom-nav :idx="0"></bottom-nav>
+ </div>
 </template>
 
 <script>
-export default {
+  import bottomNav from '@/components/bottomNav'  //底部导航
+  export default {
   name: 'App',
   data(){
       return{
-        // headMsg:'wodedingbujjjjjj
-        selected: 'my'
+
       }
   },
-
-  watch: {
-    selected: function (val, oldVal) {
-      // 这里就可以通过 val 的值变更来确定
-      console.log(val)
-    }
-  },
-
+  components:{
+      'bottom-nav':bottomNav
+  }
 
 }
 </script>
@@ -66,39 +35,13 @@ export default {
   text-align: center;
 }
 .content{
-  margin-top: 0.4rem;
+  margin-top: 0.8rem;
 }
-  .mint-tabbar{
-    height: 0.65rem;
-    background: #3c3c3c;
-  }
-  .mint-tab-item-label{
-    color: #fff;
-  }
 .mint-header{
-  background-color: #333;
-  font-size: 0.16rem;
-  color: #fff;
+  background-color: #333333;
 }
-.mint-tabbar > .mint-tab-item.is-selected {
-  background-color: #3c3c3c;
-  color: #06b1f8;
+.mint-header-title{
+  font-size: 0.34rem;
 }
 
-.mint-tab-item-label {
-  color: #fff;
-  background: #06b1f8;
-  height: 0.2rem;
-  line-height: 0.2rem;
-}
-/* .mint-tabbar > .mint-tab-item.is-selected>.mint-tab-item-icon{ */
-  /*margin-top: -0.05rem;*/
-/* } */
-/*.mint-tabbar > .mint-tab-item.is-selected>.mint-tab-item-label{*/
-  /*color: #fff;*/
-  /*background: #06b1f8;*/
-  /*border-top: 10px solid #fff;*/
-  /*border-radius:0.15rem 0.15rem 0 0;*/
-  /*margin-top: -0.05rem;*/
-/*}*/
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/index/Index'
-import my from '@/components/my/my'   //我的页面
+
 import eatPage from '@/components/index/eatPage'
+import my from '@/components/my/my'   //我的页面
 import order from '@/components/my/order'   //我的订单页面
+import myInfo from '@/components/my/myInfo'   //我的信息详情页面
+import notify from '@/components/my/notify'   //我的 通知 页面
 
 Vue.use(Router)
 
@@ -28,6 +31,16 @@ export default new Router({
       path: '/order',
       name: 'order',
       component: order
+    },
+    {
+      path: '/myInfo',
+      name: 'myInfo',
+      component: myInfo
+    },
+    {
+      path: '/notify',
+      name: 'notify',
+      component: notify
     }
   ]
 })
